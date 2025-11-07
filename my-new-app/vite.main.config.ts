ae6c0c08c562e02d2d9fa4aba6f3b500eaad4624
Rach573 <158048435+Rach573@users.noreply.github.com>
2025-11-07 19:08:38 +0100

    Update external dependencies in Vite config

--- my-new-app/vite.main.config.ts
+++ my-new-app/vite.main.config.ts
@@ -1,10 +1,17 @@
+// my-new-app/vite.main.config.ts
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
 export default defineConfig({
 	build: {
 		rollupOptions: {
-			external: ['mysql2', 'mysql2/promise']
+			// Externaliser prisma, son adaptateur, et mysql2
+			external: [
+        '@prisma/client', 
+        '@prisma/adapter-mariadb', 
+        'mysql2', 
+        'mysql2/promise'
+      ]
 		}
 	}
 });
